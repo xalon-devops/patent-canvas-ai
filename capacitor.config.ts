fd46b1774572ff5be1485014f342397f5ed9f5a2
xalon-devops <devops@xalon.ai>
2026-01-07 17:15:44 +0000

    Add Capacitor and native platforms

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.patentcanvas.ai',
+  appName: 'Patent Canvas AI',
+  webDir: 'dist'
+};
+
+export default config;
